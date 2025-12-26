d367545c63022cc00794412c0f35636645e5cf38
anecha <anecha@anechas-MacBook-Pro.local>
2025-12-26 20:07:18 +0700

    others file

---
+++ basic/test2.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>ตัวอย่างการใช้ PHP</title>
+</head>
+<body>
+    <% echo ("This is the PHP Command"\n); %>
+</body>
+</html>
+
+<!-- ก็คือไม่ควรใช้ echo แบบนี้ ให้ tag php เฉพาะส่วนนที่ต้องการมันเร็วกว่า -->
+
+<!-- <?php echo "<h3>echo คือ การแสดงผลออกทางหน้าจอของ PHP</h3>"; ?>
+    <?php echo "echo คือ การแสดงผลออกทางหน้าจอของ PHP"; ?> 
+    <script language="php"> รูปแบบการในี้ไม่ยอมรับใน PHP เวอร์ชั่นใหม่
+        echo "<h3>echo คือ การแสดงผลออกทางหน้าจอของ PHP</h3>";
+        echo "echo คือ การแสดงผลออกทางหน้าจอของ PHP";
+    </script>-->
---
+++ basic/x.php
@@ -0,0 +1,10 @@
+<HTML>
+<HEAD><TITLE>ตัวอย่างการเขียน php แบบ c programming/ pearl style</TITLE></HEAD>
+<BODY> 
+<?php echo "TEST COMMENT C++ STYLE";                         // การ Comments แบบ C++
+echo "TEST COMMENT C++ STYLE";                         // การ Comments แบบ C++
+echo "TEST COMMENT C  STYLE ";                         /* การ Comments แบบ C */
+echo "TEST COMMENT PERL STYLE ";                        # การ Comments แบบ Perl
+?>
+</BODY>
+</HTML>
